d226ac3caa9d2640c59815c5229285bb83401b34
kevincarbon123 <kevfunny@hotmail.com>
2023-04-04 02:11:23 -0400

    Added browser detection

--- Project/Iter3/faq.php
+++ Project/Iter3/faq.php
@@ -146,6 +146,23 @@
                         <!-- End FAQ Item -->
                 </div>
         </div>
-</body>
+        <?php 
+                function brdetect()
 
+                {
+                $res = $_SERVER['HTTP_USER_AGENT'];
+                #echo $res . "<br/><br/>";
+                if (strpos($res, "Chrome") == true)
+                        echo "Browser: Google Chrome";
+                else if (strpos($res, "Firefox") == true)
+                        echo "Browser: Firefox";
+                else if (strpos($res, "Safari") == true)
+                        echo "Browser: Safari";
+                else if (strpos($res, "Trident") == true)
+                        echo "Browser: Internet Explorer";
+                else
+                        echo "Browser: unkown";
+                }
+                brdetect();
+        ?>
 </html>
